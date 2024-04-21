76b5fc57a1b2c865d6cba6d361b305de6407ec0f
Nithin Pillai <61659869+bipitybop101@users.noreply.github.com>
2024-04-20 20:22:49 -0400

    created the carousel

--- currently/src/app/components/vertical-carousel/vertical-carousel.component.ts
+++ currently/src/app/components/vertical-carousel/vertical-carousel.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Reel } from '../../model/Reel';
 import { LoadReelService } from '../../services/load-reel.service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-vertical-carousel',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './vertical-carousel.component.html',
   styleUrl: './vertical-carousel.component.css'
 })
